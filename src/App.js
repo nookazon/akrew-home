926fcb3315b78029e8c57868c017c98a348966b4
Daniel Luu <danielqluu@gmail.com>
2020-12-15 11:07:54 -0500

    mountain

--- src/App.js
+++ src/App.js
@@ -12,27 +12,44 @@ function App() {
           <img src={logo} className='App-logo' alt='logo' />
           Akrew
         </h1>
-        <div>Expanding the player experience beyond the game</div>
       </header>
-      <div className='brands'>
-        <div className='brand'>
-          <a href='https://nookazon.com' target='blank'>
-            <img src={nookazon} alt='Nookazon' />
-          </a>
-        </div>
-        <div className='brand'>
-          <a href='https://traderie.com' target='blank'>
-            <img src={traderie} alt='Traderie' />
-          </a>
-        </div>
-        <div className='brand'>
-          <a href='https://drippit.com' target='blank'>
-            <img src={drippit} alt='Drippit' />
-          </a>
+      <div className='body'>
+        <div className='container'>
+          <div style={{ padding: 40, fontSize: 20 }}>
+            Expanding the player experience beyond the game
+          </div>
+          <h2>Our Brands</h2>
+          <div className='brands'>
+            <div className='brand'>
+              <a href='https://nookazon.com' target='blank'>
+                <img src={nookazon} alt='Nookazon' />
+              </a>
+            </div>
+            <div className='brand'>
+              <a href='https://traderie.com' target='blank'>
+                <img src={traderie} alt='Traderie' />
+              </a>
+            </div>
+            <div className='brand'>
+              <a href='https://drippit.com' target='blank'>
+                <img src={drippit} alt='Drippit' />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-      <div>
-        <a href='mailto:hello@akrew.com'>Contact Us</a>
+      <div className='footer'>
+        <div className='container'>
+          <div style={{ marginBottom: 15 }}>
+            <a
+              href='mailto:hello@akrew.com'
+              style={{ color: 'black', fontWeight: 'bold' }}
+            >
+              Contact Us
+            </a>
+          </div>
+          <div>© 2020 Akrew Inc.</div>
+        </div>
       </div>
     </div>
   )
